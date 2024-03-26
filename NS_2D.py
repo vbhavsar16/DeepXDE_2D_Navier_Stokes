2395f120701fc3f4a0a8afb625a7837c2e314a38
vbhavsar16 <imvivek1695@gmail.com>
2024-03-25 20:09:54 -0400

    Setting up a problem

--- NS_2D.py
+++ NS_2D.py
@@ -12,14 +12,18 @@ L = 2
 geom = dde.geometry.Rectangle(xmin=[-L/2,-D/2], xmax=[L/2,D/2])                       # Rectangle xmin = the minimum point in for both the direction. xmax is the maximum for both direction.
 
 def boundary_wall(X, on_boundary):                                                    # This function takesin points X = (x,y) and whether it is on boundary or not (on_boundary is a dde built-in. When the random points are generated, dde mark the boundary points.).
-    print("X", X)                                                                     # So, we know if they are on boundaries, but not which one. We don't want the points on inlet and outlet boundaries. Only on the wall boundaries.
-    print("on_boundary", on_boundary)
+    #print("X", X)                                                                     # So, we know if they are on boundaries, but not which one. We don't want the points on inlet and outlet boundaries. Only on the wall boundaries.
+    #print("on_boundary", on_boundary)
     
-    on_wall = np.logical_and                                                            # Logical function gives either true or false, on_wall = 1/0, 1= on wall boundary, 0 = not (Boundary point but not on wall).
+    on_wall = np.logical_and(np.logical_or(np.isclose(X[1], -D/2, rtol=1e-05, atol=1e-08), np.isclose(X[1], D/2, rtol=1e-05, atol=1e-08)), on_boundary)
     
     '''
+    Logical function gives either true or false, on_wall = 1/0, 1= on wall boundary, 0 = not (Maybe a Boundary point but not on wall).
+    
     np.logical_and is a (&& - and kind) of condition (both argument should satisfy), np.logical_or is a (|| - or kind) of condition (Either of them satisfy then true).
     In a and argument ( or argument (either on top wall or bottom wall = 1), points on boundary = 1), if those both are 1 then on_wall = 1.
+    
+    np.isclose(a,b) gives true/false, true-> if a is close to b in the tolerance given. Here in this problem, X=(x,y) so X[1] is the value on y axis. We dont care for x value, just figuring if X[1] is close to -D/2 or D/2, then it is on wall boundary.
     '''
     
     return on_wall
